bd2d13fae8b9412cf62126f3c83b007b25d6dc6b
Ryan Erkal <ryanerkal@yahoo.ie>
2024-02-21 14:44:18 +0000

    image domain

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-      // NextJS <Image> component needs to whitelist domains for src={}
-      "lh3.googleusercontent.com",
-      "pbs.twimg.com",
-      "images.unsplash.com",
-      "logos-world.net",
-    ],
-  },
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**"
+			}
+		]
+	}
 };
 
 module.exports = nextConfig;
